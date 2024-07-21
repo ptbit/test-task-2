8488b32bddb2694cd8b867de57b715f3f0e0369e
foxstudent103376 <yashopua@gmail.com>
2024-07-21 12:57:21 +0300

    add solution

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('script');
+const paragraph = document.querySelector('.paragraph');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+const clearBtn = document.querySelector('.clear');
+
+window.addEventListener('load', () => {
+  const localData = localStorage.getItem('num23');
+  paragraph.textContent = localData;
+
+  if (+localData % 2 === 0) {
+    paragraph.style.color = 'green';
+  } else {
+    paragraph.style.color = 'red';
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  if (input.value === '') {
+    e.preventDefault();
+  } else {
+    localStorage.setItem('num23', input.value);
+  }
+});
+
+clearBtn.addEventListener('click', (e) => {
+  form.submit();
+  input.value = '';
+  localStorage.removeItem('num23');
+});
+
+input.addEventListener('input', (e) => {
+  clearBtn.disabled = e.target.value === '';
+});
